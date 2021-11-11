8183efbed1c717b3b13161b8876495299524378b
Felipe Pinto <srfps7@gmail.com>
2021-11-11 11:22:01 -0300

    Mudanças na organização

--- Dissertação/Norte do Chile/aldean_trauma.R
+++ Dissertação/Norte do Chile/aldean_trauma.R
@@ -12,13 +12,18 @@ show(dados)
 
 
 #Matrizes 2x2 de comparagco Masculino vs Feminino por periodo:
-
+ARC <- matrix(c(31,12,100,111), nr=2, dimnames = list(c("masculino", "feminino"), c("afetado", "nao afetado")))
 
 fisher.test(ARC)
+
 
 F <- matrix(c(38,8,115,127), nr=2,  dimnames = list(c("masculino", "feminino"),
                                                     c("afetado", "nco afetado")))
 fisher.test(F)
+
+Fi <- matrix(c(8,38,127,115), nr=2,  dimnames = list(c("masculino", "feminino"),
+                                                    c("afetado", "nco afetado")))
+fisher.test(Fi)
 
 EIP <- matrix(c(30,20,222,151), nr=2,  dimnames = list(c("masculino", "feminino"),
                                                        c("afetado", "nco afetado")))
@@ -49,6 +54,10 @@ ARC_F_M <- matrix(c(31,38,100,115), nr=2,  dimnames = list(c("Periodo anterior",
                                                            c("Afetado", "Nco afetado")))
 fisher.test(ARC_F_M)
 
+ARC_F_Mi <- matrix(c(38,31,115,100), nr=2,  dimnames = list(c("Periodo F", "Periodo ARC"),
+                                                           c("Afetado", "Nco afetado")))
+fisher.test(ARC_F_Mi)
+
 F_EIP_M <-matrix(c(38,30,115,222), nr=2, dimnames = list(c("Periodo anterior", "Periodo posterior"),
                                                          c("Afetado", "Nco afetado")))
 fisher.test(F_EIP_M)
@@ -57,9 +66,21 @@ EIP_MH_M <- matrix(c(30,113,222,535), nr=2, dimnames = list(c("Periodo anterior"
                                                             c("Afetado", "Nco afetado")))
 fisher.test(EIP_MH_M)
 
+#invertendo a ordem de compara????o quando a frequencia do periodo posterior ?? maior 
+EIP_MH_Mi <- matrix(c(113,30,535,222), nr=2, dimnames = list(c("Periodo MH", "Periodo EIP"),
+                                                            c("Afetado", "Nco afetado")))
+fisher.test(EIP_MH_Mi)
+
+
 MH_LIP_M <- matrix(c(113,460,535,1079), nr=2, dimnames = list(c("Periodo anterior", "Periodo posterior"),
                                                               c("Afetado", "Nco afetado")))
 fisher.test(MH_LIP_M)
+
+#invertendo a ordem de compara????o quando a frequencia do periodo posterior ?? maior 
+
+MH_LIP_Mi <- matrix(c(460,113,1079,535), nr=2, dimnames = list(c("Periodo LIP", "Periodo MH"),
+                                                              c("Afetado", "Nco afetado")))
+fisher.test(MH_LIP_Mi)
 
 LIP_LH_M <- matrix(c(460,230,1079,1025), nr=2, dimnames = list(c("Periodo anterior", "Periodo posterior"),
                                                                c("Afetado", "Nco afetado")))
@@ -75,6 +96,10 @@ F_EIP_F <- matrix(c(8,20,127,151), nr=2, dimnames = list(c("Periodo anterior", "
                                                          c("Afetado", "Nco afetado")))
 fisher.test(F_EIP_F)
 
+F_EIP_Fi <- matrix(c(20,8,151,127), nr=2, dimnames = list(c("Periodo EIP", "Periodo F"),
+                                                         c("Afetado", "Nco afetado")))
+fisher.test(F_EIP_Fi)
+
 EIP_MH_F <- matrix(c(20,62,151,696), nr=2, dimnames = list(c("Periodo anterior", "Periodo posterior"),
                                                            c("Afetado", "Nco afetado")))
 fisher.test(EIP_MH_F)
@@ -82,6 +107,10 @@ fisher.test(EIP_MH_F)
 MH_LIP_F <- matrix(c(62,313,696,853), nr=2, dimnames = list(c("Periodo anterior", "Periodo posterior"),
                                                             c("Afetado", "Nco afetado")))
 fisher.test(MH_LIP_F)
+
+MH_LIP_Fi <- matrix(c(313,62,853,696), nr=2, dimnames = list(c("Periodo LIP", "Periodo MHr"),
+                                                            c("Afetado", "Nco afetado")))
+fisher.test(MH_LIP_Fi)
 
 LIP_LH_F <- matrix(c(313,112,853,821), nr=2, dimnames = list(c("Periodo anterior", "Periodo posterior"),
                                                              c("Afetado", "Nco afetado")))
